4e7e7b74b310bd6f45700777591bb24668bed4fb
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2024-01-17 19:49:51 +0530

    created NavMoreOption component and also made navigation resposive i.e to be different on different screen size

--- src/layout/NavigationBar/NavigationBar.jsx
+++ src/layout/NavigationBar/NavigationBar.jsx
@@ -1,22 +1,84 @@
 // import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProfilePic from "../../components/ProfilePic";
 import NavMessengerDropDown from "./components/NavMessengerDropDown";
 import NavNotificationDropdown from "./components/NavNotificationDropdown";
 import NavProfileDropdown from "./components/NavProfileDropdown";
 import NavCreateDropDown from "./components/NavCreateDropDown";
 import NavSearchDropDown from "./components/NavSearchDropDown";
+import NavMoreOptions from "./components/NavMoreOptions";
 
 const NavigationBar = () => {
   const [showDropDownOf, setShowDropDownOf] = useState("");
   const [searchActive, setSearchActive] = useState(false);
+  const [navHomeButtonActive, setNavHomeButtonActive] = useState(true);
+  const [navVideoButtonActive, setNavVideoButtonActive] = useState(false);
+  const [navFriendsButtonActive, setNavFriendsButtonActive] = useState(false);
+  const [navGroupsButtonActive, setNavGroupsButtonActive] = useState(false);
+  const [showMoreOptions, setShowMoreOptions] = useState(false);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  const [search, setSearch] = useState("");
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+
   const showDropDownOfHandler = (value) => {
     setShowDropDownOf(value);
+  };
+
+  const showMoreOptionsHandler = () => {
+    setShowMoreOptions((state) => !state);
   };
 
   const searchActiveHandler = () => {
     setSearchActive((state) => !state);
   };
+
+  const searchInputHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const searchInputFocusHandler = () => {
+    setIsSearchFocused(true);
+  };
+
+  const searchInputBlurHandler = () => {
+    setIsSearchFocused(false);
+  };
+
+  const navHomeButtonActiveHandler = () => {
+    setNavHomeButtonActive(true);
+    setNavFriendsButtonActive(false);
+    setNavGroupsButtonActive(false);
+    setNavVideoButtonActive(false);
+  };
+  const navFriendsButtonActiveHandler = () => {
+    setNavHomeButtonActive(false);
+    setNavFriendsButtonActive(true);
+    setNavGroupsButtonActive(false);
+    setNavVideoButtonActive(false);
+  };
+  const navGroupsButtonActiveHandler = () => {
+    setNavHomeButtonActive(false);
+    setNavFriendsButtonActive(false);
+    setNavGroupsButtonActive(true);
+    setNavVideoButtonActive(false);
+  };
+  const navVideoButtonActiveHandler = () => {
+    setNavHomeButtonActive(false);
+    setNavFriendsButtonActive(false);
+    setNavGroupsButtonActive(false);
+    setNavVideoButtonActive(true);
+  };
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [windowSize]);
+
   return (
     <div className="w-full flex bg-white justify-between py-2 px-5 shadow-md">
       <div className="flex items-center">
@@ -25,23 +87,111 @@ const NavigationBar = () => {
           src="src/assets/fb-logo.png"
           alt="logo"
         />
-        <div className="cursor-pointer">
+        {windowSize < 768 ? (
+          <>
+            <div className="cursor-pointer">
+              <div
+                onClick={searchActiveHandler}
+                className="h-[2.5rem] w-[2.5rem] flex items-center justify-center rounded-full bg-gray-100"
+              >
+                <span className="text-[1.5rem] text-gray-500 material-symbols-outlined">
+                  search
+                </span>
+              </div>
+              {searchActive && (
+                <NavSearchDropDown searchActiveHandler={searchActiveHandler} />
+              )}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="flex gap-4 w-full justify-between items-center outline-none">
+              {isSearchFocused && (
+                <NavSearchDropDown
+                  searchActiveHandler={searchInputBlurHandler}
+                />
+              )}
+
+              <input
+                className="px-6 py-2 w-full outline-none rounded-full bg-gray-100"
+                type="text"
+                value={search}
+                onChange={searchInputHandler}
+                onFocus={searchInputFocusHandler}
+                placeholder="Search Facebook"
+              />
+            </div>
+          </>
+        )}
+        {windowSize < 1024 && (
           <div
-            onClick={searchActiveHandler}
-            className="h-[2.5rem] w-[2.5rem] flex items-center justify-center rounded-full bg-gray-100"
+            className={`ml-6 flex items-center ${
+              showMoreOptions && "border-b-2 border-primary-color"
+            }`}
           >
-            <span className="text-[1.5rem] text-gray-500 material-symbols-outlined">
-              search
+            <span
+              onClick={showMoreOptionsHandler}
+              className={`text-[2rem] ${
+                showMoreOptions && "text-primary-color"
+              } cursor-pointer text-gray-500 material-symbols-outlined`}
+            >
+              menu
             </span>
+            {showMoreOptions && <NavMoreOptions />}
           </div>
-          {searchActive && (
-            <NavSearchDropDown searchActiveHandler={searchActiveHandler} />
-          )}
-        </div>
-        <span className="pl-6 text-[2rem] text-gray-500 material-symbols-outlined">
-          menu
-        </span>
+        )}
       </div>
+
+      {windowSize >= 1024 && (
+        <div className=" mt-2">
+          <ul
+            className={`flex justify-between items-center ${
+              windowSize > 1424 ? "gap-40" : "gap-24"
+            } mr-16`}
+          >
+            <li onClick={navHomeButtonActiveHandler}>
+              <span
+                className={`text-[1.8rem] cursor-pointer ${
+                  navHomeButtonActive &&
+                  "border-b-2 border-primary-color text-primary-color"
+                } material-symbols-outlined`}
+              >
+                home
+              </span>
+            </li>
+            <li onClick={navFriendsButtonActiveHandler}>
+              <span
+                className={`text-[1.8rem] cursor-pointer ${
+                  navFriendsButtonActive &&
+                  "border-b-2 border-primary-color text-primary-color"
+                } material-symbols-outlined`}
+              >
+                group
+              </span>
+            </li>
+            <li onClick={navVideoButtonActiveHandler}>
+              <span
+                className={`text-[1.8rem] cursor-pointer ${
+                  navVideoButtonActive &&
+                  "border-b-2 border-primary-color text-primary-color"
+                } material-symbols-outlined`}
+              >
+                video_library
+              </span>
+            </li>
+            <li onClick={navGroupsButtonActiveHandler}>
+              <span
+                className={`text-[1.8rem] cursor-pointer ${
+                  navGroupsButtonActive &&
+                  "border-b-2 border-primary-color text-primary-color"
+                } material-symbols-outlined`}
+              >
+                groups
+              </span>
+            </li>
+          </ul>
+        </div>
+      )}
       <div className="dropdown dropdown-end">
         <div className="flex gap-2">
           <div
---
+++ src/layout/NavigationBar/components/NavMoreOptions.jsx
@@ -0,0 +1,66 @@
+import ProfilePic from "../../../components/ProfilePic";
+
+const NavMoreOptions = () => {
+  return (
+    <div className="z-[3] absolute top-14 left-0 w-[100vw] h-[93.5vh] bg-[#f0f2f5]">
+      <div className="mt-4 mx-4">
+        <ul>
+          <li className="flex gap-3 items-center px-4 py-2 rounded-lg cursor-pointer hover:bg-gray-200">
+            <ProfilePic
+              imgPath={
+                "https://scontent.fdel63-1.fna.fbcdn.net/v/t39.30808-6/362671347_829206822078167_18836369532297524_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=K0M7vYT3crIAX8CRxEN&_nc_ht=scontent.fdel63-1.fna&oh=00_AfBDA4Ml9iF_oKcg24DWIs-qmvC2wVYz-S99yybI6iPsKw&oe=65A6C8C1"
+              }
+              height={"2.5rem"}
+              width={"2.5rem"}
+            />
+            <p>Abhishek Maurya</p>
+          </li>
+          <li className="flex gap-3 items-center mt-1 px-4 py-3 rounded-lg cursor-pointer hover:bg-gray-200">
+            <span className="text-[2.2rem] material-symbols-outlined">
+              group
+            </span>
+            <p>Find Friends</p>
+          </li>
+          <li className="flex gap-3 items-center mt-1 px-4 py-3 rounded-lg cursor-pointer hover:bg-gray-200">
+            <span className="text-[2.2rem] material-symbols-outlined">
+              bookmark
+            </span>
+            <p>Saved</p>
+          </li>
+          <li className="flex gap-3 items-center mt-1 px-4 py-3 rounded-lg cursor-pointer hover:bg-gray-200">
+            <span className="text-[2.2rem] material-symbols-outlined">
+              groups
+            </span>
+            <p>Groups</p>
+          </li>
+          <li className="flex gap-3 items-center mt-1 px-4 py-3 rounded-lg cursor-pointer hover:bg-gray-200">
+            <span className="text-[2.2rem] material-symbols-outlined">
+              chat
+            </span>
+            <p className="pb-1">Messenger</p>
+          </li>
+          <li className="flex gap-3 items-center mt-1 px-4 py-3 rounded-lg cursor-pointer hover:bg-gray-200">
+            <span className="text-[2.2rem] material-symbols-outlined">
+              video_library
+            </span>
+            <p>Videos</p>
+          </li>
+          <li className="flex gap-3 items-center mt-1 px-4 py-3 rounded-lg cursor-pointer hover:bg-gray-200">
+            <span className="text-[2.2rem] material-symbols-outlined">
+              feed
+            </span>
+            <p>Feeds</p>
+          </li>
+          <li className="flex gap-3 items-center mt-1 px-4 py-3 rounded-lg cursor-pointer hover:bg-gray-200">
+            <span className="text-[2.2rem] material-symbols-outlined">
+              event_available
+            </span>
+            <p>Events</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavMoreOptions;
