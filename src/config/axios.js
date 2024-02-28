e393e44035888d8a315928e2330cc901dfe6f80e
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2024-02-28 11:13:00 +0530

    vercel deployment config fixed

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// import { isJwtTokenExpired } from "../utils/isJwtTokenExpired";
-// import store from "../redux/store";
-// import { logout } from "../redux/reducers/authSlice";
+import { isJwtTokenExpired } from "../utils/isJwtTokenExpired";
+import store from "../redux/store";
+import { logout } from "../redux/reducers/authSlice";
 
 const axiosConfig = axios.create({
   baseURL: "https://socialappbackend.vercel.app/api/v1/",
@@ -12,28 +12,28 @@ const axiosConfig = axios.create({
   withCredentials: true,
 });
 
-// axiosConfig.interceptors.request.use(async (config) => {
-//   try {
-//     const refreshToken = sessionStorage.getItem("refresh-token");
-//     if (refreshToken === null) return config;
-//     if (isJwtTokenExpired(refreshToken)) {
-//       // call logout function
-//       const accessToken = localStorage.getItem("access-token");
-//       await axios({
-//         method: "post",
-//         url: "https://socialappbackend.vercel.app/api/v1/logout",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Bearer ${accessToken}`,
-//         },
-//         withCredentials: true,
-//       });
-//       store.dispatch(logout());
-//     }
-//     return config;
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// });
+axiosConfig.interceptors.request.use(async (config) => {
+  try {
+    const refreshToken = sessionStorage.getItem("refresh-token");
+    if (refreshToken === null) return config;
+    if (isJwtTokenExpired(refreshToken)) {
+      // call logout function
+      const accessToken = localStorage.getItem("access-token");
+      await axios({
+        method: "post",
+        url: "https://socialappbackend.vercel.app/api/v1/logout",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        withCredentials: true,
+      });
+      store.dispatch(logout());
+    }
+    return config;
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 export default axiosConfig;
